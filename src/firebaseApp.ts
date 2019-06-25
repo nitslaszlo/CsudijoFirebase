54a5b5d11b0c5a555e7e8be32ee20e21b82d4de3
nitslaszlo <nitslaszlo@gmail.com>
2019-06-25 18:54:21 +0200

    up

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -14,9 +14,5 @@ const config = {
 
 const app = firebase.initializeApp(config);
 const db = app.firestore();
-// The timestampsInSnapshots setting now defaults to true and you no
-// longer need to explicitly set it. In a future release, the setting
-// will be removed entirely and so it is recommended that you remove it
-// from your firestore.settings() call now.
-// db.settings({ timestampsInSnapshots: true });
+
 export default db;
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import router from "./router";
 import BootstrapVue from "bootstrap-vue";
 import { firestorePlugin } from "vuefire";
 
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
